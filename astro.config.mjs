f9ed31937de286a0a13c9e0f02a9907e6da53955
Steven Siahaan <stevenjhsiahaan@gmail.com>
2025-03-10 15:45:18 +0700

    Initial Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://yourusername.github.io',
+	base: '/',
 	integrations: [tailwind()],
 });
